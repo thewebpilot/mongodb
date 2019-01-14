9ab0e760954dae524755b4fd9f11c566093db0fd
anusha <anusha.m314@gmail.com>
2019-01-13 23:17:53 -0600

    added connection.js file

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect('mongodb://localhost/testaroo');
 mongoose.connection.once('open',function(){
-  console.log("Connection haxs been made");
+  console.log("Connection has been made");
 }).on('error',function(){
   console.log("Connection error:",error);``
 });
---
+++ test/demo_test.js
@@ -0,0 +1,8 @@
+const mocha = require("mocha");
+const assert = require("assert");
+
+describe("Demo Tests",function(){
+  it("Add two numbers ",function(){
+    assert(5+2===7);
+  });
+});
